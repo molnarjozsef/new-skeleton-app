cdaa38a0971da54b67d5e9d955c5842cf64b2098
Jozsef Molnar <jmolnar@anwb.nl>
2022-12-09 01:47:34 +0100

    feat: add open url button to details page

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/navigation/DetailsScreen.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/navigation/DetailsScreen.kt
@@ -2,11 +2,18 @@
 
 package com.jozsefmolnar.newskeletonapp.navigation
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -29,7 +36,10 @@ fun DetailsScreen(item: StateFlow<Article?>) {
 }
 
 @Composable
-fun DetailsScreenContent(article: Article) {
+fun DetailsScreenContent(
+    article: Article,
+    context: Context = LocalContext.current,
+) {
     Box(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -58,8 +68,28 @@ fun DetailsScreenContent(article: Article) {
                     text = article.description ?: "DESCRIPTION",
                     style = AppTypography.bodyLarge,
                 )
+
+                Spacer(Modifier.height(Sizes.Size200))
+
+                Button(
+                    onClick = { context.openUrl(article.url) },
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Text(
+                        text = "Open in Browser",
+                    )
+                }
             }
         }
+    }
+}
+
+private fun Context.openUrl(url: String) {
+    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    try {
+        startActivity(browserIntent)
+    } catch (_: ActivityNotFoundException) {
+        Toast.makeText(this, "Browser not found", Toast.LENGTH_LONG).show()
     }
 }
 
@@ -68,3 +98,4 @@ fun DetailsScreenContent(article: Article) {
 fun DetailsPreview() {
     DetailsScreenContent(article = ArticleGenerator.generateArticle())
 }
+
