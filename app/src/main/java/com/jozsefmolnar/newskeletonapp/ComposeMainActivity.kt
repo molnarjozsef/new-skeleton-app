c3fa4e44dc2c21084d5a85fb6c48d89b527443be
Jozsef Molnar <jmolnar@anwb.nl>
2022-12-09 17:25:31 +0100

    feat: add navbar and background color

--- app/src/main/java/com/jozsefmolnar/newskeletonapp/ComposeMainActivity.kt
+++ app/src/main/java/com/jozsefmolnar/newskeletonapp/ComposeMainActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.jozsefmolnar.newskeletonapp.navigation.Navigation
 import com.jozsefmolnar.newskeletonapp.ui.model.MainViewModel
 import com.jozsefmolnar.newskeletonapp.ui.theme.NewSkeletonAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,9 +24,19 @@ class ComposeMainActivity() : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel.fetchLatestNews()
+
         setContent {
-            Navigation()
+            NewSkeletonAppTheme() {
+                val systemUiController = rememberSystemUiController()
+                systemUiController.setStatusBarColor(
+                    color = MaterialTheme.colorScheme.background,
+                    darkIcons = !isSystemInDarkTheme(),
+                )
+
+                Box(Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    Navigation()
+                }
+            }
         }
     }
 }
